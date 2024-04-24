8f4537daf955b01738b390fccca37ffb156cacc8
João Paulo N. Rodrigues <joaopaulonr.cloud@gmail.com>
2024-04-24 11:42:14 -0300

    feat: add private subnet

---
+++ infra/sb_private.tf
@@ -0,0 +1,25 @@
+resource "aws_subnet" "eks_subnet_private_1a" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
+  availability_zone = "${data.aws_region.current_region.name}a"
+  tags = merge(
+    local.tags, {
+      Name                              = "jotacloud-sb-private-1a",
+      "kubernetes.io/role/internal-elb" = 1
+    }
+
+  )
+}
+
+resource "aws_subnet" "eks_subnet_private_1b" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 4)
+  availability_zone = "${data.aws_region.current_region.name}b"
+  tags = merge(
+    local.tags, {
+      Name                              = "jotacloud-sb-private-1a",
+      "kubernetes.io/role/internal-elb" = 1
+    }
+
+  )
+}
