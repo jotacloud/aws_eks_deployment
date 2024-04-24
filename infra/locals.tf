f4fd0cb07c51cdecac82cda25afc71cd45dd8359
João Paulo N. Rodrigues <joaopaulonr.cloud@gmail.com>
2024-04-24 11:42:46 -0300

    feat: add tags

---
+++ infra/locals.tf
@@ -0,0 +1,9 @@
+locals {
+  tags = {
+    Department   = "Cloud&DevSecOps"
+    Organization = "Infrastrructure and Operations"
+    Project      = "EKS"
+    Enviroment   = "Development"
+    Owner        = "Terraform"
+  }
+}
