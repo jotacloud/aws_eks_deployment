b4e8ef10bf24cdfc9cb72f820297385d616ee83c
João Paulo N. Rodrigues <joaopaulonr.cloud@gmail.com>
2024-04-24 10:33:34 -0300

    feat: add remote backend

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.46.0"
     }
+  }
+  backend "s3" {
+    bucket = "jotacloud-terraform-state"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
